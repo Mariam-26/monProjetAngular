a3605d22482e3f5ab7048798c20cf64d1e5f03d3
Mariam <82433410+Mariam-26@users.noreply.github.com>
2023-01-08 18:11:23 +0100

    Create mon-projet-angular.model.ts

---
+++ monprojetangular/src/app/models/mon-projet-angular.model.ts
@@ -0,0 +1,23 @@
+export class monPojetAngular {
+  title!: string;
+  description!: string;
+  imageUrl!: string;
+  createdDate!: Date;
+  projet!: number;
+  location?: string;
+
+  // constructor(
+  //   public title: string,
+  //   public description: string,
+  //   public imageUrl: string,
+  //   public createdDate: Date,
+  //   public projet: number,
+  //   public location?: string
+  // ) {
+  //   // this.title = title;
+  //   // this.description = description;
+  //   // this.imageUrl = imageUrl;
+  //   // this.createdDate = createdDate;
+  //   // this.projet = projet;
+  // }
+}
