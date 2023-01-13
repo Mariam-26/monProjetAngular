9a129efa696038168512cf674f355b25dff9d842
Mariam <82433410+Mariam-26@users.noreply.github.com>
2023-01-13 13:39:20 +0100

    Update app.component.ts

--- monprojetangular/src/app/app.component.ts
+++ monprojetangular/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { monPojetAngular } from './models/mon-projet-angular.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,60 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  monPojetAngulars!: monPojetAngular[];
 
   ngOnInit() {
-    this.monPojetAngulars = [
-      {
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis toute petite !!!',
-        imageUrl:
-          'https://cdn.pixabay.com/photo/2018/08/11/22/53/teddy-bear-3599680_1280.jpg',
-        createdDate: new Date(),
-        projet: 0,
-        location: 'Paris',
-      },
-      {
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis toute petite !!!',
-        imageUrl:
-          'https://cdn.pixabay.com/photo/2018/08/11/22/53/teddy-bear-3599680_1280.jpg',
-        createdDate: new Date(),
-        projet: 0,
-        location: 'la montagne',
-      },
-      {
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis toute petite !!!',
-        imageUrl:
-          'https://cdn.pixabay.com/photo/2018/08/11/22/53/teddy-bear-3599680_1280.jpg',
-        createdDate: new Date(),
-        projet: 0,
-      },
-      {
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis toute petite !!!',
-        imageUrl:
-          'https://cdn.pixabay.com/photo/2018/08/11/22/53/teddy-bear-3599680_1280.jpg',
-        createdDate: new Date(),
-        projet: 0,
-      },
-      {
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis toute petite !!!',
-        imageUrl:
-          'https://cdn.pixabay.com/photo/2018/08/11/22/53/teddy-bear-3599680_1280.jpg',
-        createdDate: new Date(),
-        projet: 0,
-      },
-      {
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis toute petite !!!',
-        imageUrl:
-          'https://cdn.pixabay.com/photo/2018/08/11/22/53/teddy-bear-3599680_1280.jpg',
-        createdDate: new Date(),
-        projet: 0,
-      },
-    ];
+
   }
 }
