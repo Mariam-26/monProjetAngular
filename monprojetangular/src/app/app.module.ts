3f2bfed068528e78e804228cadf2acfbc6f00ffe
Mariam <82433410+Mariam-26@users.noreply.github.com>
2023-01-13 13:39:24 +0100

    Update app.module.ts

--- monprojetangular/src/app/app.module.ts
+++ monprojetangular/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AppRoutingModule } from './app-routing.module';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
@@ -5,13 +6,20 @@ import * as fr from '@angular/common/locales/fr';
 
 import { AppComponent } from './app.component';
 import { MonProjetAngularComponent } from './mon-projet-angular/mon-projet-angular.component';
+import { MonProjetListComponent } from './mon-projet-list/mon-projet-list.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
-  declarations: [AppComponent, MonProjetAngularComponent],
-  imports: [BrowserModule],
-  providers: [
-    { provide: LOCALE_ID, useValue: 'fr-FR'
-  }],
+  declarations: [
+    AppComponent,
+    MonProjetAngularComponent,
+    MonProjetListComponent,
+    HeaderComponent,
+    LandingPageComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {
