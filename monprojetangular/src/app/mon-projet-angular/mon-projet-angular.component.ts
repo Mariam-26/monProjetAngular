0b6f11a2afb5b68ac8f4de341a75e3584f72cce1
Mariam <82433410+Mariam-26@users.noreply.github.com>
2023-01-14 19:44:57 +0100

    Update mon-projet-angular.component.ts

--- monprojetangular/src/app/mon-projet-angular/mon-projet-angular.component.ts
+++ monprojetangular/src/app/mon-projet-angular/mon-projet-angular.component.ts
@@ -1,7 +1,8 @@
+//On voit ici la construction du component. On voit une classe TypeScript qui s'appelle projetComponent, avec un  constructor  et une méthode  ngOnInit. Dès maintenant, dans le fichier  mon-projet-angular.component.ts, supprimez : le contenu de la classe (le constructor et la méthode  ngOnInit) ; la partie "implements OnInit" de la déclaration de classe ;l'importOnInitdepuis  @angular/core  (laissez bien Component).
+
 import { MonProjetService } from './../services/mon-projets.service';
 import { monPojetAngular } from './../models/mon-projet-angular.model';
 import { Component, OnInit, Input } from '@angular/core';
-
 @Component({
   selector: 'app-mon-projet-angular',
   templateUrl: './mon-projet-angular.component.html',
@@ -10,6 +11,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class MonProjetAngularComponent implements OnInit {
   @Input() monPojetAngular!: monPojetAngular;
   buttonText!: string;
+
+// Un décorateur en TypeScript permet, entre autres, d'apporter des modifications à une classe. Ici, le décorateur  @Component  vient ajouter tous les comportements nécessaires à l'utilisation de ce component dans l'application. Il est importé depuis le package  @angular/core. Tout se passe sous le capot, on n'a pas à s'en occuper !
 
   constructor(private monProjetService: MonProjetService) {}
 
