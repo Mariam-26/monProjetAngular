cd9582bcb50a3bb740ce4403b22d05e2c78ca5ea
Mariam <82433410+Mariam-26@users.noreply.github.com>
2023-02-19 11:25:32 +0100

    Update app.component.ts

--- monprojetangular/src/app/app.component.ts
+++ monprojetangular/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+// APP.COMPONENT LA RACINE DE MES COMPONENT
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
+// J'AI SUPPRIMER LA VARIBLE title
+
 export class AppComponent implements OnInit {
 
-  ngOnInit() {
+  ngOnInit()/* METHODE DE OnInit */ {
 
   }
 }
