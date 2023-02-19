8b508cc7ceeffd8b3fc01ef292e30341be99a124
Mariam <82433410+Mariam-26@users.noreply.github.com>
2023-02-19 11:25:56 +0100

    Update mon-projet-angular.component.ts

--- monprojetangular/src/app/mon-projet-angular/mon-projet-angular.component.ts
+++ monprojetangular/src/app/mon-projet-angular/mon-projet-angular.component.ts
@@ -2,14 +2,14 @@
 
 import { MonProjetService } from './../services/mon-projets.service';
 import { monPojetAngular } from './../models/mon-projet-angular.model';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input /* Input  doit être ajouté aux imports depuis  @angular/core  ! */} from '@angular/core';
 @Component({
   selector: 'app-mon-projet-angular',
   templateUrl: './mon-projet-angular.component.html',
   styleUrls: ['./mon-projet-angular.component.scss'],
 })
 export class MonProjetAngularComponent implements OnInit {
-  @Input() monPojetAngular!: monPojetAngular;
+  @Input()/* Pour qu'une propriété puisse être injectée depuis l'extérieur d'un component, il faut lui ajouter le décorateur  @Input() */ monPojetAngular!: monPojetAngular;
   buttonText!: string;
 
 // Un décorateur en TypeScript permet, entre autres, d'apporter des modifications à une classe. Ici, le décorateur  @Component  vient ajouter tous les comportements nécessaires à l'utilisation de ce component dans l'application. Il est importé depuis le package  @angular/core. Tout se passe sous le capot, on n'a pas à s'en occuper !
@@ -17,10 +17,10 @@ export class MonProjetAngularComponent implements OnInit {
   constructor(private monProjetService: MonProjetService) {}
 
   ngOnInit() {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
     this.buttonText = 'Oh Projet !';
   }
+
+  // FONCTION POUR AJOUTER OU SOUSTRAIRE UN LIKE
   onProjet() {
     if (this.buttonText === 'Oh Projet !') {
       this.monProjetService.projetMonPojetAngularById(
